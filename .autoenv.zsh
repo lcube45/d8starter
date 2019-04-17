60049d4edce1ffe949f8a9afdce7c99df64ff4c8
lcube <info@lcube.fr>
2019-04-17 19:44:29 +0200

    add zsh local alias via autoenv plugin

---
+++ .autoenv.zsh
@@ -0,0 +1,10 @@
+autostash TOOLS_IN_DOCKER=1
+autostash DOCKER_PHP_CONTAINER=univsandbox_php_1
+
+composer() {
+    if [ "$TOOLS_IN_DOCKER" = "1" ]; then
+        docker-compose run --rm --entrypoint composer --workdir=/var/www/html php "$@"
+    else
+        command composer "$@"
+    fi
+}
